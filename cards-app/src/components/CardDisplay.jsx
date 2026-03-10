b19025d46e32e49fa737bab4c3985a750321c6dc
Bobb-93 <borisiakimov@gmail.com>
2026-03-10 13:45:39 +0200

    added background of the back image

--- cards-app/src/components/CardDisplay.jsx
+++ cards-app/src/components/CardDisplay.jsx
@@ -2,36 +2,33 @@ import { useState, useEffect } from 'react';
 import './CardDisplay.css';
 
 function CardDisplay({ card, label, shouldFlip }) {
-    
+
     const [flipped, setFlipped] = useState(false);
-    
-    useEffect( () => {
-        if(shouldFlip && card){
+
+    useEffect(() => {
+        if (shouldFlip && card) {
             const timer = setTimeout(() => setFlipped(true), 50);
             return () => clearTimeout(timer);
         }
     }, [card, shouldFlip]);
 
     return (
-
         <div className="card-container">
             <h3>{label}</h3>
 
             <div className={`card ${flipped ? 'flipped' : ''}`}>
-                <div className="card-face card-back">
-                    <p>No card</p>
-                </div>
-
+                <div className="card-face card-back"></div>
 
                 <div className="card-face card-front">
                     {card && (
-                        <>
-                            <img src={card.image} alt={`${card.value} of ${card.suit}`} />
-                            <p className='card-title'>{card.value} of {card.suit}</p>
-                        </>
+                        <img src={card.image} alt={`${card.value} of ${card.suit}`} />
                     )}
                 </div>
             </div>
+
+            <p className="card-title">
+                {card ? `${card.value} of ${card.suit}` : '\u00A0'}
+            </p>
         </div>
     );
 }
